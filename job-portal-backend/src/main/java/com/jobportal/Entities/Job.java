58ca94480c2e966cf7ec047a989f2b8e7e529d7d
PullariArjun <arjunpullari@gmail.com>
2025-01-28 13:55:43 +0530

    Job Searching System

---
+++ job-portal-backend/src/main/java/com/jobportal/Entities/Job.java
@@ -0,0 +1,32 @@
+package com.jobportal.Entities;
+
+import com.jobportal.DTO.Applicant;
+import com.jobportal.DTO.JobStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+public class Job {
+    @Id
+    private Long id;
+    private String jobTitle;
+    private String company;
+    private List<Applicant> applicants;
+    private String about;
+    private String experience;
+    private String jobType;
+    private String location;
+    private Long packageOffered;
+    private LocalDateTime postTime;
+    private String description;
+    private List<String> skillsRequired;
+    private JobStatus jobStatus;
+}
